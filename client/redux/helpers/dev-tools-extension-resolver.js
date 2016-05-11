a4cee7ee8f3bc207f6e00658aa3d5402b042a472
Sebastian <sebastian.lindstrom@gmail.com>
2016-05-11 18:58:02 +0200

    Removed broken reducer functionality

--- client/redux/helpers/dev-tools-extension-resolver.js
+++ client/redux/helpers/dev-tools-extension-resolver.js
@@ -2,6 +2,6 @@ export default function (){
     if (window /* TODO Resolve only when in dev-mode */){
         return window.devToolsExtension ? window.devToolsExtension() : f => f
     }
-    
+
     return f => f //Redux expects function when composing reducers and middleware
 }
